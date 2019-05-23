13ef7cbb184805891fdf9584e3ca1a26b23ccdd7
やよ <yayo@yayonoMacBook-puro.local>
2019-05-23 19:06:45 +0900

    fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,12 +1,16 @@
+# import django_heroku
 import dj_database_url
 
 from .base import *
 
 
 DEBUG = False
-SECRET_KEY = '@(o)5pq(3(fj#^s_n!vik&e0o@+6ezbjpbf^8(gst@(f+z(rjb'
+ALLOWED_HOSTS = ['*']
 
+SECRET_KEY = '@(o)5pq(3(fj#^s_n!vik&e0o@+6ezbjpbf^8(gst@(f+z(rjb'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# django_heroku.settings(locals())
 DATABASES = {
     'default': dj_database_url.config()
 }
-ALLOWED_HOSTS = ['*']
